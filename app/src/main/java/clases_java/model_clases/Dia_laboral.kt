71e072ecddb3c299514863f6048b9d6e54754e75
algarcser <43754393+algarcser@users.noreply.github.com>
2022-08-29 10:16:19 +0200

    actualización del entorno de trabajo

--- app/src/main/java/clases_java/model_clases/Dia_laboral.kt
+++ app/src/main/java/clases_java/model_clases/Dia_laboral.kt
@@ -2,4 +2,5 @@ package clases_java.model_clases
 
 import java.sql.Date
 
-data class Dia_laboral(val list_actividades: List<Actividad>, val fecha: Date)
+data class Dia_laboral(val list_actividades: List<Actividad>,
+                       val fecha: Date)
